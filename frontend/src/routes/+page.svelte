1f942aa4d0743f345efe81e528ef90e67ce9f1eb
ciara <quinnc33@tcd.ie>
2026-09-14 18:11:40 +0100

    start adding basics, svelte, fastapi

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to EngSim!</h1>
+<p>This is a simple simulation for software engineering projects. You will act as a senior software engineer participating in task triage, code reviews, and project planning.</p>
